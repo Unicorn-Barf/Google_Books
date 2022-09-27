af10c846619aa32af6ebe04f95f2fb9c7631b6cf
Unicorn Barf <nolan.spence7@gmail.com>
2022-09-27 14:26:09 -0700

    Fixed connection database url.

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
-const secret = 'mysecretsshhhhh';
+const secret = process.env.SESSION_SECRET || 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
